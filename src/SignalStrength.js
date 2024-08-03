4c0763725925fa52b0085e6b0c898f4bb5aea807
rtglabs <rtglaboratories@gmail.com>
2024-08-03 11:56:39 -0500

    Delete src directory

--- src/SignalStrength.js
+++
@@ -1,9 +0,0 @@
-// components/SignalStrength.js
-import React from 'react';
-
-const SignalStrength = () => {
-  // Placeholder for the Signal Strength logic
-  return <div>Signal Strength Indicator</div>;
-};
-
-export default SignalStrength;
